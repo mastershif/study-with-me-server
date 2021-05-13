272b5a312de542e6349e58e7201232cc2b97d853
mastershif <shifra@amenityanalytics.com>
2021-05-13 19:27:45 +0300

    await the updates of a user and a group and execute the findByIdAndUpdate query

--- routes/joinGroup.js
+++ routes/joinGroup.js
@@ -4,7 +4,7 @@ const router = express.Router();
 let Group = require("../models/group").Group;
 const { User } = require("../models/user");
 
-router.post("/", async(request, response) => {
+router.put("/", async(request, response) => {
     try {
         const user = await User.findOne({ email: request.body.email }).select('groups');
         if (user === null) {
@@ -16,8 +16,8 @@ router.post("/", async(request, response) => {
         }
         const group = await Group.findById(mongoose.Types.ObjectId(request.body.groupId));
         // add the new group to the user's groups
-        User.findByIdAndUpdate(user._id, { groups: [...userGroups.groups, group._id] }, { strict: false });
-        Group.findByIdAndUpdate(group._id, { users: [...group.users, user._id] }, { strict: false });
+        await User.findByIdAndUpdate(user._id, { groups: [...userGroups.groups, group._id] }, { strict: false }).exec();
+        await Group.findByIdAndUpdate(group._id, { users: [...group.users, user._id] }, { strict: false }).exec();
         response.status(200).json({ message: `User ${request.body.email} has joined the group ${group.groupTitle}`});
     } catch (error) {
         console.log(error);
