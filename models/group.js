761c52de76b79ea48011f99e248c4da50f3f2989
mastershif <shifra@amenityanalytics.com>
2021-05-13 19:25:41 +0300

    use object id type for users and admin fields on a group

--- models/group.js
+++ models/group.js
@@ -16,8 +16,8 @@ const groupSchema = new Schema({
     place: String,
     link: String,
     calendar: Boolean,
-    users: [String],
-    admin: String
+    users: [Schema.Types.ObjectId],
+    admin: Schema.Types.ObjectId
 });
 
 const Group = mongoose.model("Group", groupSchema);
