4a41b9b09e5af14fb2c0ea024e903b20f032436d
Roee <roy_has@yahoo.com>
2021-05-18 11:36:18 +0300

    now when a group is deleted, it is deleted for all users also

--- routes/deleteGroup.js
+++ routes/deleteGroup.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
-let Group = require("../models/group").Group;
+let User = require("../models/user").User;
+const Group = require("../models/group").Group;
 
 router.delete("/:groupID", async(request, response) => {
     try {
         const groupID = request.params.groupID;
+        const group = await Group.findOne({ _id: groupID })
+        const groupUsers = await User.find({ _id: { $in: group.users } });
+        for (i = 0; i < groupUsers.length; i++) {
+            await User.updateOne({ _id: groupUsers[i]._id }, { $pull: { 'groups': groupID } });
+        }
         await Group.deleteOne({ _id: groupID })
         response.status(200).json({ message: `Group ${groupID} was deleted successfully` });
     } catch (error) {
